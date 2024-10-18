1c32a44f03019112325195ae07c14a0af0233f3c
Jonathan Her <jhawj1@hotmail.com>
2024-10-18 13:34:06 -0500

    Testing AWS pipeline

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 // Open up endpoints
+app.get("/", "I updated");
 app.use("/tickets", ticketRouter);
 app.use("/users", userRouter);
 app.use("/login", loginRouter);
